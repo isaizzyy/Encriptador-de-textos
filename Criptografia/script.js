33dcdd3ca9bf9fea9b75808f0d2b30ab3e5fb57e
Isabelle <114663286+isaizzyy@users.noreply.github.com>
2024-08-01 09:44:05 -0300

    Add files via upload

---
+++ Criptografia/script.js
@@ -0,0 +1,70 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+// As "chaves" de criptografia que utilizaremos são:
+// 'a' -> 'ai'
+// 'e' -> 'enter'
+// 'i' -> 'imes'
+// 'o' -> 'ober'
+// 'u' -> 'ufat'
+
+function btnEncriptar() {
+    const textEncriptado = encriptar(textArea.value);
+    mensagem.value = textEncriptado;
+    textArea.value = "";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+
+}
+
+function btnCopiarTexto() {
+    mensagem.select();
+    mensagem.setSelectionRange(0, 99999); // Para dispositivos móveis
+    navigator.clipboard.writeText(mensagem.value).then(() => {
+        alert("Copiado com sucesso!");
+    }).catch(err => {
+        console.error('Erro ao copiar texto: ', err);
+    });
+}
